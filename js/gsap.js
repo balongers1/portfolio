741b11f52ff2e6862857825e3a8d37e0604ff5a0
unknown <balongers1@gmail.com>
2021-04-01 14:42:33 -0700

    April 1 2021

---
+++ js/gsap.js
@@ -0,0 +1,16 @@
+
+// tweenmax
+ var tl = gsap.timeline({defaults:{duration: 1}})
+        
+      tl.from(".anim1",1,{ opacity: 1, scale: "-100%",ease:Expo.easeInOut})
+        .to(".anim1",1, {opacity: 0,   y:"-100%",  ease:Expo.easeInOut})
+        .to(".overlay",1,{opacity: 1, y: "-100%", ease:Expo.easeInOut})
+        .from(".nav-title",.2,{ opacity: 0, y:"-15%",ease:Expo.ease})
+        .from(".nav1",.2,{ opacity: 0, y:"-15%",ease:Expo.ease})
+        .from(".nav2",.2,{ opacity: 0, y:"-15%",ease:Expo.ease})
+        .from(".nav3",.2,{ opacity: 0, y:"-15%",ease:Expo.ease})
+        .from(".nav4",.2,{ opacity: 0, y:"-15%",ease:Expo.ease})
+        .from(".c1",.3,{ opacity: 0, y:"15%",ease:Expo.ease})
+        .from(".c2",.3,{ opacity: 0, y:"15%",ease:Expo.ease})
+        .from(".c3",.3,{ opacity: 0, y:"15%",ease:Expo.ease})
+        
---
+++ js/swiper.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop:true,
+    freeMode: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
