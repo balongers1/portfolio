fbef756f67d496f32505232d381f441a5a03b9c3
unknown <balongers1@gmail.com>
2021-06-15 04:59:35 -0700

    work2

--- js/gsap.js
+++ js/gsap.js
@@ -2,7 +2,7 @@
 // tweenmax
  var tl = gsap.timeline({defaults:{duration: 1}})
         
-      tl.from(".anim1",1,{ opacity: 1, scale: "-100%",ease:Expo.easeInOut})
+      tl.from(".anim1",1,{ opacity: 1, y: "50%",ease:Expo.easeInOut})
         .to(".anim1",1, {opacity: 0,   y:"-100%",  ease:Expo.easeInOut})
         .to(".overlay",1,{opacity: 1, y: "-100%", ease:Expo.easeInOut})
         .from(".nav-title",.2,{ opacity: 0, y:"-15%",ease:Expo.ease})
